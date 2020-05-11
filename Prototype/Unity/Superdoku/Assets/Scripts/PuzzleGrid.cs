53f9af490d70969f08e7b0b5cc6509536a415f87
Brett Allen <brettallen777@gmail.com>
2020-05-10 20:15:28 -0400

    Begin working on integrating unity implementation

---
+++ Prototype/Unity/Superdoku/Assets/Scripts/PuzzleGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Superdoku
+{
+    public class PuzzleGrid : MonoBehaviour
+    {
+        [Header("Cells")]
+        public GameObject[] cells;
+
+        private GameObject m_currActiveCell;
+        private GameObject m_prevActiveCell;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnCellSelected(GameObject cell) 
+        {
+            Debug.Log("Selected cell: " + cell);
+        }
+    }
+}
