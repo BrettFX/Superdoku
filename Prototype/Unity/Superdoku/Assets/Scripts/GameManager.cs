42dd3d9556616573c9d16b401cb82511ccb5f3da
Brett Allen <brettallen777@gmail.com>
2020-05-14 23:44:18 -0400

    Fix ui scaling Looking into creating a new input field for mobile support

--- Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
+++ Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
@@ -8,7 +8,32 @@ namespace Superdoku
 {
     public class GameManager : MonoBehaviour
     {
-        private InputField m_currActiveCell;
+        // Static Refs //
+        private static GameManager instance;
+        public static GameManager Instance { get { return instance; } }
+
+        private string m_currActiveCellName;
+
+        void Awake()
+        {
+            // If the instance variable is already assigned...
+            if (instance != null)
+            {
+                // If the instance is currently active...
+                if (instance.gameObject.activeInHierarchy == true)
+                {
+                    // Warn the user that there are multiple Game Managers within the scene and destroy the old manager.
+                    Debug.LogWarning("There are multiple instances of the Game Manager script. Removing the old manager from the scene.");
+                    Destroy(instance.gameObject);
+                }
+
+                // Remove the old manager.
+                instance = null;
+            }
+
+            // Assign the instance variable as the Game Manager script on this object.
+            instance = GetComponent<GameManager>();
+        }
 
         // Start is called before the first frame update
         void Start()
@@ -22,10 +47,37 @@ namespace Superdoku
 
         }
 
-        public void OnCellSelected(InputField cell)
+        public void OnCellSelected(GameObject cell)
         {
-            Debug.Log("Selected cell: " + cell);
-            m_currActiveCell = cell;
+            Debug.Log("Selected cell: " + cell.name);
+            this.m_currActiveCellName = cell.name;
+        }
+
+        public void OnNumberClicked(GameObject button)
+        {
+            // Grab the respective number value of the clicked button
+            NumberButton numberButton = button.GetComponent<NumberButton>();
+            Debug.Log("Button clicked: " + numberButton.value);
+
+            // Set the text of the currently selected input field based on the number that has been clicked
+            if (this.m_currActiveCellName != null)
+            {
+                // Lookup by name
+                GameObject gameObject = GameObject.Find(this.m_currActiveCellName);
+                InputField targetInputField = gameObject.GetComponentInChildren<InputField>();
+
+                Debug.Log(gameObject);
+                Debug.Log(targetInputField);
+
+                targetInputField.text = numberButton.value.ToString();
+
+                // Highlight the input field again since it lost focus
+                targetInputField.ActivateInputField();
+            }
+            else
+            {
+                Debug.Log("Must select a cell first!");
+            }
         }
 
         // See: https://forum.unity.com/threads/unity-4-6-ui-how-to-prevent-deselect-losing-focus-in-inputfield.272387/
---
+++ Prototype/Unity/Superdoku/Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Superdoku
+{
+    public class ObjectClicker : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.transform != null)
+                    {
+                        //Debug.Log("Clicked: " + hit.transform.gameObject.name);
+                        // Delegate to the on cell select function defined in game manager
+                        GameManager.Instance.OnCellSelected(hit.transform.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
