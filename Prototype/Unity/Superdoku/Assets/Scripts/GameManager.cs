4b3e06ca44eed2068773e36ac524304bf968f54a
Brett Allen <brettallen777@gmail.com>
2020-05-22 00:05:42 -0400

    Implement ability to story ui puzzle cells into 2d integer array Currently can solve puzzles from initial state; although the solved puzzle is not yet displayed.
    Next steps are to implement ability to update 2d integer array in real
    time in Update function of PuzzleGrid so that the puzzle can be solved
    in any state

--- Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
+++ Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
@@ -4,148 +4,177 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace Superdoku
-{
-    public class GameManager : MonoBehaviour
-    {
-        // Static Refs //
-        private static GameManager instance;
-        public static GameManager Instance { get { return instance; } }
+namespace Superdoku {
+	public class GameManager: MonoBehaviour {
+		// Static Refs //
+		private static GameManager instance;
+		public static GameManager Instance {
+			get {
+				return instance;
+			}
+		}
 
-        [Header("Cell Management")]
-        public Color defaultColor = Color.white;
-        public Color selectionColor = Color.blue;
+		[Header("Cell Management")]
+		public Color defaultColor = Color.white;
+		public Color selectionColor = Color.blue;
 
-        private bool m_selected = false;
+		private bool m_selected = false;
+        private bool m_solved = false;
 
-        private Color[] m_colorWheel;
+		private Color[] m_colorWheel;
 
-        private static GameObject m_currentActiveCell;
+		private static GameObject m_currentActiveCell;
 
-        void Awake()
-        {
-            // If the instance variable is already assigned...
-            if (instance != null)
-            {
-                // If the instance is currently active...
-                if (instance.gameObject.activeInHierarchy == true)
-                {
-                    // Warn the user that there are multiple Game Managers within the scene and destroy the old manager.
-                    Debug.LogWarning("There are multiple instances of the Game Manager script. Removing the old manager from the scene.");
-                    Destroy(instance.gameObject);
-                }
+		void Awake() {
+			// If the instance variable is already assigned...
+			if (instance != null) {
+				// If the instance is currently active...
+				if (instance.gameObject.activeInHierarchy == true) {
+					// Warn the user that there are multiple Game Managers within the scene and destroy the old manager.
+					Debug.LogWarning("There are multiple instances of the Game Manager script. Removing the old manager from the scene.");
+					Destroy(instance.gameObject);
+				}
 
-                // Remove the old manager.
-                instance = null;
-            }
+				// Remove the old manager.
+				instance = null;
+			}
 
-            // Assign the instance variable as the Game Manager script on this object.
-            instance = GetComponent<GameManager>();
+			// Assign the instance variable as the Game Manager script on this object.
+			instance = GetComponent < GameManager > ();
+		}
+
+		// Start is called before the first frame update
+		void Start() {
+			// Selection color wheel for toggling between selected and deselected
+			m_colorWheel = new Color[2] {
+				defaultColor,
+				selectionColor
+			};
+
+            PuzzleGrid.Instance.Show();
+		}
+
+		// Update is called once per frame
+		void Update() {
+
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            // Selection color wheel for toggling between selected and deselected
-            m_colorWheel = new Color[2]
-            {
-                defaultColor,
-                selectionColor
-            };
-        }
+		public void OnCellSelected(GameObject cell) {
+			Debug.Log("Selected cell: " + cell.name);
 
-        // Update is called once per frame
-        void Update()
-        {
+			Button button = cell.GetComponent < Button > ();
+			m_currentActiveCell = cell;
+		}
 
-        }
+		public void OnNumberClicked(GameObject button) {
+			// Grab the respective number value of the clicked button
+			Text buttonText = button.GetComponentInChildren < Text > ();
 
-        public void OnCellSelected(GameObject cell)
-        {
-            Debug.Log("Selected cell: " + cell.name);
+			// Set the text of the currently selected input field based on the number that has been clicked
+			if (m_currentActiveCell != null) {
+				Button targetCell = m_currentActiveCell.GetComponent < Button > ();
+				Debug.Log(targetCell);
 
-            Button button = cell.GetComponent<Button>();
-            m_currentActiveCell = cell;
-        }
+				targetCell.GetComponentInChildren < Text > ().text = buttonText.text;
 
-        public void OnNumberClicked(GameObject button)
-        {
-            // Grab the respective number value of the clicked button
-            Text buttonText = button.GetComponentInChildren<Text>();
+				// TODO change respective element value of 2d sudoku array (e.g., bind cells to elements)
 
-            // Set the text of the currently selected input field based on the number that has been clicked
-            if (m_currentActiveCell != null)
-            {
-                Button targetCell = m_currentActiveCell.GetComponent<Button>();
-                Debug.Log(targetCell);
+				// Highlight the target cell since it lost focus
+				targetCell.Select();
+			} else {
+				Debug.Log("Must select a cell first!");
+			}
+		}
 
-                targetCell.GetComponentInChildren<Text>().text = buttonText.text;
+		public void OnSolve() {
+			Debug.Log("Solving puzzle...");
 
-                // TODO change respective element value of 2d sudoku array (e.g., bind cells to elements)
+            long startTime, endTime;
 
+            Debug.Log("Here is the problem:\n");
+            PuzzleGrid.Instance.Show();
 
-                // Highlight the target cell since it lost focus
-                targetCell.Select();
-            }
-            else
-            {
-                Debug.Log("Must select a cell first!");
-            }
-        }
+            startTime = (long)(Time.deltaTime * 1000);
 
-        public void OnSolve()
-        {
-            Debug.Log("Solving puzzle...");
+            //Begin the SuDoKu-solving algorithm at the beginning of the 9x9 matrix
+            PuzzleGrid.Instance.SolvePuzzle(0, 0);
+
+            Debug.Log("\nHere is the solution:\n");
+            PuzzleGrid.Instance.Show();
+
+            endTime = (long)(Time.deltaTime * 1000);
+            Debug.Log("Solution took " + (endTime - startTime) + " millisecond(s) to derive.");
 
             // Deselect current active cell
             m_currentActiveCell = null;
-        }
+		}
 
-        public void OnCamera()
+        public void SetSolved(bool b)
         {
-            Debug.Log("Launching camera scene...");
-
-            // Deselect current active cell
-            m_currentActiveCell = null;
+            m_solved = b;
         }
 
-        // See: https://forum.unity.com/threads/unity-4-6-ui-how-to-prevent-deselect-losing-focus-in-inputfield.272387/
-        void ReactivateInputField(InputField inputField)
+        public bool IsSolved()
         {
-            if (inputField != null)
-            {
-                StartCoroutine(ActivateInputFieldWithoutSelection(inputField));
-            }
+            return m_solved;
         }
 
-        IEnumerator ActivateInputFieldWithoutSelection(InputField inputField)
-        {
-            inputField.ActivateInputField();
-            // wait for the activation to occur in a lateupdate
-            yield return new WaitForEndOfFrame();
-            // make sure we're still the active ui
-            if (EventSystem.current.currentSelectedGameObject == inputField.gameObject)
-            {
-                // To remove hilight we'll just show the caret at the end of the line
-                inputField.MoveTextEnd(false);
-            }
-        }
+		public void OnCamera() {
+			Debug.Log("Launching camera scene...");
 
-        /**
+			// Deselect current active cell
+			m_currentActiveCell = null;
+		}
+
+		// See: https://forum.unity.com/threads/unity-4-6-ui-how-to-prevent-deselect-losing-focus-in-inputfield.272387/
+		void ReactivateInputField(InputField inputField) {
+			if (inputField != null) {
+				StartCoroutine(ActivateInputFieldWithoutSelection(inputField));
+			}
+		}
+
+		IEnumerator ActivateInputFieldWithoutSelection(InputField inputField) {
+			inputField.ActivateInputField();
+			// wait for the activation to occur in a lateupdate
+			yield
+			return new WaitForEndOfFrame();
+			// make sure we're still the active ui
+			if (EventSystem.current.currentSelectedGameObject == inputField.gameObject) {
+				// To remove hilight we'll just show the caret at the end of the line
+				inputField.MoveTextEnd(false);
+			}
+		}
+
+		/**
+        * Clears all the text field cells in the 9x9 matrix
+        * */
+		public void ClearCells() {
+			m_currentActiveCell = null;
+
+
+			//if (superdokuAnchor != null) {
+			//	for (Node node: superdokuAnchor.getChildren()) {
+			//		if (node instanceof TextField) { ((TextField) node).clear();
+			//			((TextField) node).setEditable(true);
+			//		}
+			//	}
+			//}
+		}
+
+		/**
         * Toggle the selection color of a game object based on two colors to cycle between
         * @param GameObject obj the game object to extract the renderer from and alter the material color
         */
-        private void ToggleSelectionColor(GameObject obj)
-        {
-            // Toggle selection state (change before processing for onClick change)
-            m_selected = !m_selected;
+		private void ToggleSelectionColor(GameObject obj) {
+			// Toggle selection state (change before processing for onClick change)
+			m_selected = !m_selected;
 
-            // Choose the respective color from the color wheel based on the selection state
-            Color color = m_colorWheel[m_selected ? 1 : 0];
+			// Choose the respective color from the color wheel based on the selection state
+			Color color = m_colorWheel[m_selected ? 1 : 0];
 
-            // Obtain the renderer from the game object and set its material color
-            Renderer rend = obj.GetComponent<Renderer>();
-            rend.material.color = color;
-        }
-    }
+			// Obtain the renderer from the game object and set its material color
+			Renderer rend = obj.GetComponent < Renderer > ();
+			rend.material.color = color;
+		}
+	}
 }
--- Prototype/Unity/Superdoku/Assets/Scripts/PuzzleGrid.cs
+++ Prototype/Unity/Superdoku/Assets/Scripts/PuzzleGrid.cs
@@ -20,6 +20,7 @@ namespace Superdoku
         [Header("Cells")]
         //public Row[] cells;
         public GameObject[] cellRows;
+        private int[,] sudokuPuzzle;
 
         void Awake()
         {
@@ -45,41 +46,206 @@ namespace Superdoku
         // Start is called before the first frame update
         void Start()
         {
-            Show();
+            sudokuPuzzle = new int[9,9];
+            InitializePuzzle();
         }
 
         // Update is called once per frame
         void Update()
         {
+            // Keep the sudoku puzzle integer 2d array in sync with ui cells
 
         }
 
-        private void Show()
+        /**
+	    * Sets all contents of the sudokuPuzzle to zero and clears all text fields as long as
+	    * they are loaded
+	    * */
+        public void InitializePuzzle()
         {
-            // String to build while iterating sudoku puzzle for displaying/logging
-            string displayStr = "";
-
-            for (int row = 0; row < cellRows.Length; row++)
+            for (int x = 0; x < 9; x++)
             {
-                Button[] cellButtons = cellRows[row].GetComponentsInChildren<Button>();
-
-                for (int col = 0; col < cellButtons.Length; col++)
+                for (int y = 0; y < 9; y++)
                 {
-                    string btnText = cellButtons[col].GetComponentInChildren<Text>().text;
-
-                    // Get integer representation
-                    int btnValue = btnText.Equals("") ? 0 : Convert.ToInt32(btnText);
-
-                    //Print out a divider every third line
-                    displayStr += (col % 3 == 0) ? "\t" : " ";
-                    displayStr += btnValue.ToString();
-                    //Debug.Log("Cell at (" + row + ", " + col + ") value: " + btnValue);
+                    sudokuPuzzle[x, y] = 0;
                 }
-
-                displayStr += ((row + 1) % 3 == 0) ? "\n" : "";
             }
 
-            Debug.Log(displayStr);
+            GameManager.Instance.ClearCells();
+
+            GameManager.Instance.SetSolved(false);
+
+            Debug.Log("Puzzle initialized");
+        }
+
+        /**
+	    * Determines if the number to be inserted has not already been used in the current row
+	    * 
+	    * @param p the two-dimensional 9x9 puzzle
+	    * @param row the row to be determined if the number to be inserted is valid
+	    * @param num the number to determine is valid
+	    * @return whether the number to be inserted is valid or not
+	    * */
+        public bool FollowsRowRule(int row, int num)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                //If a number in the row already exists then the number in question does not satisfy the row rule
+                if (sudokuPuzzle[row, y] == num)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /**
+         * Determines if the number to be inserted has not already been used in the current column
+         * 
+         * @param p the two-dimensional 9x9 puzzle
+         * @param col the column of the puzzle to be determined if valid
+         * @param num the number to determine is valid
+         * @return whether the number to be inserted is valid or not
+         * */
+        public bool FollowsColRule(int col, int num)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                //If a number in the column already exists then the number in question does not satisfy the column rule
+                if (sudokuPuzzle[x, col] == num)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * Determines if the number to be inserted has not already been used in the 3x3 subspace
+         * that the current cell lives in
+         * 
+         * @param p the two-dimensional 9x9 puzzle
+         * @param row the row in the 3x3 subspace of the puzzle to be determined if valid
+         * @param col the column in the 3x3 subspace of the puzzle to be determined if valid
+         * @param num the number to determine is valid
+         * @return whether the number to be inserted is valid within the 3x3 subspace or not
+         * */
+        public bool FollowsSquareRule(int row, int col, int num)
+        {
+            int xStart, yStart;
+
+            if (row >= 6)
+            {
+                xStart = 6;
+            }
+            else if (row >= 3)
+            {
+                xStart = 3;
+            }
+            else
+            {
+                xStart = 0;
+            }
+
+            if (col >= 6)
+            {
+                yStart = 6;
+            }
+            else if (col >= 3)
+            {
+                yStart = 3;
+            }
+            else
+            {
+                yStart = 0;
+            }
+
+            for (int x = xStart; x < xStart + 3; x++)
+            {
+                for (int y = yStart; y < yStart + 3; y++)
+                {
+                    if (x != row && y != col)
+                    {
+                        if (sudokuPuzzle[x, y] == num)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /**
+	    * Driver method to solve the SuDoKu puzzle.
+	    * Recursively determines the correct numbers for each cell
+	    * 
+	    * @param the two-dimensional 9x9 puzzle to be solved 
+	    * @param row the current row within the 9x9 SuDoKu puzzle
+	    * @param col the current column  within the 9x9 SuDoKu puzzle
+	    * */
+        public void SolvePuzzle(int row, int col)
+        {
+
+            if (col > 8)
+            {
+                col = 0;
+                row++;
+            }
+
+            //Puzzle solved (we reached the last cell without any errors)
+            if (row > 8 || GameManager.Instance.IsSolved())
+            {
+                GameManager.Instance.SetSolved(true);
+                return;
+            }
+
+            if (sudokuPuzzle[row, col] == 0)
+            {
+                //Try each number 1-9 until it satisfies all three rules
+                for (int num = 1; num <= 9; num++)
+                {
+                    if (FollowsRowRule(row, num) && FollowsColRule(col, num) && FollowsSquareRule(row, col, num))
+                    {
+                        sudokuPuzzle[row, col] = num;
+                        SolvePuzzle(row, col + 1);
+
+                        if (GameManager.Instance.IsSolved())
+                            return;
+
+                        sudokuPuzzle[row, col] = 0;
+                    }
+                }
+            }
+            else
+            {
+                //Skip cells that already have numbers 1-9
+                SolvePuzzle(row, col + 1);
+            }
+
+        }
+
+        public void Show()
+        {
+            // String to build while iterating sudoku puzzle for displaying/logging
+            string display = "";
+
+            //Use the values from the sudokuPuzzle and write them to the respective cell using the txtFieldMap
+            for (int x = 0; x < 9; x++)
+            {
+                display += "\n";
+                for (int y = 0; y < 9; y++)
+                {
+                    // Print out a divider every third line
+                    display += ((y % 3 == 0) ? "\t" : " ") + sudokuPuzzle[x, y].ToString();
+                }
+
+                display += ((x + 1) % 3 == 0) ? "\n" : "";
+            }
+
+            Debug.Log(display);
         }
     }
 }
