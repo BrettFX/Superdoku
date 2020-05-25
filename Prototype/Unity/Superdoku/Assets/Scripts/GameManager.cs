d4eadf10382ab801419a8a3cde02b4ed376dca42
Brett Allen <brettallen777@gmail.com>
2020-05-25 14:09:07 -0400

    Download/Import SimpleFileManager Create PermissionsManager script that may or may not be used going forward
    Permissions are handled by default with plugins but it seems that when
    they are prompted, it causes a bug with the camera on Android
    
    Need to look into a bug on android camera that is causeing the image to
    be sideways

--- Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
+++ Prototype/Unity/Superdoku/Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using SimpleFileBrowser;
+using System.Collections;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -56,6 +58,11 @@ namespace Superdoku {
             instance = GetComponent < GameManager > ();
         }
 
+        void Start()
+        {
+            InitializeFileBrowser();
+        }
+
         /**
         * Sets all contents of the sudokuPuzzle to zero and clears all text fields as long as
         * they are loaded
@@ -66,6 +73,51 @@ namespace Superdoku {
             PuzzleGrid.Instance.Clear();
             SetSolved(false);
             if (DEBUG_MODE) { Debug.Log("Puzzle initialized"); }
+        }
+        
+        /**
+         * Perform setup tasks for SimpleFileBrowser plugin to prepare for loading
+         * photos
+         */
+        public void InitializeFileBrowser()
+        {
+            // Set filters (optional)
+            // It is sufficient to set the filters just once (instead of each time before showing the file browser dialog), 
+            // if all the dialogs will be using the same filters
+            FileBrowser.SetFilters(true, new FileBrowser.Filter("Images", ".jpg", ".png"), new FileBrowser.Filter("Text Files", ".txt", ".pdf"));
+
+            // Set default filter that is selected when the dialog is shown (optional)
+            // Returns true if the default filter is set successfully
+            // In this case, set Images filter as the default filter
+            FileBrowser.SetDefaultFilter(".png");
+
+            // Set excluded file extensions (optional) (by default, .lnk and .tmp extensions are excluded)
+            // Note that when you use this function, .lnk and .tmp extensions will no longer be
+            // excluded unless you explicitly add them as parameters to the function
+            FileBrowser.SetExcludedExtensions(".lnk", ".tmp", ".zip", ".rar", ".exe");
+
+            // Add a new quick link to the browser (optional) (returns true if quick link is added successfully)
+            // It is sufficient to add a quick link just once
+            // Name: Users
+            // Path: C:\Users
+            // Icon: default (folder icon)
+            FileBrowser.AddQuickLink("Users", "C:\\Users", null);
+
+            // Show a save file dialog 
+            // onSuccess event: not registered (which means this dialog is pretty useless)
+            // onCancel event: not registered
+            // Save file/folder: file, Initial path: "C:\", Title: "Save As", submit button text: "Save"
+            // FileBrowser.ShowSaveDialog( null, null, false, "C:\\", "Save As", "Save" );
+
+            // Show a select folder dialog 
+            // onSuccess event: print the selected folder's path
+            // onCancel event: print "Canceled"
+            // Load file/folder: folder, Initial path: default (Documents), Title: "Select Folder", submit button text: "Select"
+            // FileBrowser.ShowLoadDialog( (path) => { Debug.Log( "Selected: " + path ); }, 
+            //                                () => { Debug.Log( "Canceled" ); }, 
+            //                                true, null, "Select Folder", "Select" );
+
+            
         }
 
         /**
@@ -171,6 +223,8 @@ namespace Superdoku {
             modalOverlay.SetActive(false);
             if (DEBUG_MODE) { Debug.Log("Launching gallery scene..."); }
 
+            // Open file chooser dialog (using SimpleFileBrowser)
+            StartCoroutine(ShowLoadDialogCoroutine());
         }
 
         public void OnCancelModalOverlay()
@@ -268,6 +322,29 @@ namespace Superdoku {
             }
 
             return tex;
+        }
+
+        /**
+         * Coroutine for SimpleFileBrowser to show file loading dialog. Yields so that
+         * Game waits for response
+         */
+        IEnumerator ShowLoadDialogCoroutine()
+        {
+            // Show a load file dialog and wait for a response from user
+            // Load file/folder: file, Initial path: default (Documents), Title: "Load File", submit button text: "Load"
+            yield return FileBrowser.WaitForLoadDialog(false, null, "Load File", "Load");
+
+            // Dialog is closed
+            // Print whether a file is chosen (FileBrowser.Success)
+            // and the path to the selected file (FileBrowser.Result) (null, if FileBrowser.Success is false)
+            Debug.Log(FileBrowser.Success + " " + FileBrowser.Result);
+
+            if (FileBrowser.Success)
+            {
+                // If a file was chosen, read its bytes via FileBrowserHelpers
+                // Contrary to File.ReadAllBytes, this function works on Android 10+, as well
+                byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result);
+            }
         }
     }
 }
