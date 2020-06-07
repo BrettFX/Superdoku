ec9f39652d94604f6581db56ca90df395503507d
Brett Allen <brettallen777@gmail.com>
2020-06-06 22:35:58 -0400

    Attempt to create moveable and resizable puzzle overlay for image of sudoku puzzle

---
+++ Prototype/Unity/Superdoku/Assets/Scripts/Utils/ObjectDragger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ObjectDragger : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        // Pixel coordinates (x, y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // Z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 nextPos = GetMouseWorldPos() + mOffset;
+        transform.position = nextPos;
+
+        // Normalize the move position to make it valid 
+
+
+
+    }
+}
