9d9e3694005710c5603d0f907bb0c7dc86d289a1
Brett Allen <brettallen777@gmail.com>
2020-06-11 23:55:28 -0400

    Create RestRequest class that will handle image processing See https://docs.unity3d.com/Manual/UnityWebRequest-SendingForm.html for POST requests

---
+++ Prototype/Unity/Superdoku/Assets/Scripts/Network/RestRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class RestRequest : MonoBehaviour
+{
+    public void Ping()
+    {
+        // string.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", CityId, API_KEY)
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/ytc-api/converter/ping");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+
+        Debug.Log(jsonResponse);
+        //WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(jsonResponse);
+        //return info;
+    }
+}
