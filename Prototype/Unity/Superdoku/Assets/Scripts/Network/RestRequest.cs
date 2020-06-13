3de8d03effea8c20271b968c09ef3f1bd1eb690c
Brett Allen <brettallen777@gmail.com>
2020-06-13 01:00:33 -0400

    Implement test to get sudoku puzzle array from rest endpoint Converts json array to integer list in c# and then converts to 2d grid array

--- Prototype/Unity/Superdoku/Assets/Scripts/Network/RestRequest.cs
+++ Prototype/Unity/Superdoku/Assets/Scripts/Network/RestRequest.cs
@@ -1,19 +1,87 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using UnityEngine;
 
 public class RestRequest : MonoBehaviour
 {
-    public void Ping()
+    private const string BASE_URL = "http://localhost:5000/superdoku-api/{0}";
+
+    [System.Serializable]
+    public class Cell
     {
-        // string.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", CityId, API_KEY)
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/ytc-api/converter/ping");
+        public int data;
+    }
+
+    [System.Serializable]
+    public class SudokuPuzzle
+    {
+        public List<int> puzzle;
+    }
+
+    public string SendRequest(string url, string method)
+    {
+        Debug.Log("URL: " + url);
+        Debug.Log("Method: " + method);
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
-        string jsonResponse = reader.ReadToEnd();
+        return reader.ReadToEnd();
+    }
 
-        Debug.Log(jsonResponse);
-        //WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(jsonResponse);
-        //return info;
+    public void Ping()
+    {
+        // Create ping request to test server state
+        string pingUrl = string.Format(BASE_URL, "ping");
+        string response = SendRequest(pingUrl, "GET");
+        
+        Debug.Log(response);
+    }
+
+    public void Test()
+    {
+        // Test getting sample sudoku response from server
+        string response = SendRequest(string.Format(BASE_URL, "test"), "GET");
+        Debug.Log("Original Repsonse: " + response);
+        // Parse response to integer array
+        // int[] testSudoku = Array.ConvertAll(response.Split(','), int.Parse);
+        SudokuPuzzle testSudoku = JsonUtility.FromJson<SudokuPuzzle>(response);
+
+        // Get 2d version of test sudoku
+        int bounds = 9;
+        int puzzleCount = testSudoku.puzzle.Count;
+
+        Debug.Log("Rows: " + bounds + ", Columns: " + bounds);
+
+        int[,] grid = new int[bounds, bounds];
+        int i = 0;
+        for (int x = 0; x < 9; x++){
+            for (int y = 0; y < 9; y++){
+                if (i < puzzleCount)
+                {
+                    grid[x, y] = testSudoku.puzzle[i];
+                }               
+                i++;
+            }
+        }
+
+        // The character to determine formatting
+        string display = "";
+
+        for (int row = 0; row < bounds; row++)
+        {
+            display += "\n";
+
+            for (int col = 0; col < bounds; col++)
+            {
+                // Print out a divider every third line
+                display += ((col % 3 == 0) ? "\t" : " ") + grid[row, col];
+            }
+            display += ((row + 1) % 3 == 0) ? "\n" : "";
+        }
+
+        Debug.Log("\nSudoku Puzzle:");
+        Debug.Log(display);        
     }
 }
